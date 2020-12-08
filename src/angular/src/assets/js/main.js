603853ed5196857229d99713d202eb03f4a8fc5a
carlk <carlkaspar@gmail.com>
2020-12-08 13:45:18 +0200

    added fancybox jquery to controll image lightbox functionality

---
+++ src/angular/src/assets/js/main.js
@@ -0,0 +1,17 @@
+(function($){
+
+  $.fancybox.defaults.loop = true;
+  $.fancybox.defaults.wheel = false;
+  $.fancybox.defaults.image.preload = false;
+  $.fancybox.defaults.buttons = [
+    "slideShow",
+    "thumbs",
+    "close"
+  ];
+  $.fancybox.defaults.protect = true;
+  $.fancybox.defaults.clickContent = false;
+  $.fancybox.defaults.mobile = {
+    arrows: false
+  };
+
+})(jQuery);
